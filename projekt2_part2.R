6037f55d814617d882d06e3b5fc7ac16cf4cff32
Apricott <krol.michal99@gmail.com>
2020-06-13 22:06:33 +0200

    Forward chaining algorithm added

--- projekt2_part2.R
+++ projekt2_part2.R
@@ -1,44 +1,63 @@
 
-##
-## NA RAZIE NIE MA TU NIC PEWNEGO, TYLKO TESTY JAK TO MOZE DZIALAC 
+## 
 ## NOTA 1: dobrze dobrany zbior danych by duzo pomogl xD
+
+is.contained <- function(list1,list2) {
+# check if list1 is contained in list2
+  x <- FALSE
+  if (length(list2) > 0) {
+    if (length(list1) > 0) {
+      for (i in 1:length(list2)) {
+        x <- TRUE
+        for (j in 1:length(list1)) {
+          x <- (list1[[j]] %in% list2[[i]]) & x
+        }
+        if(x == TRUE) {
+          break
+        }
+      }
+    }
+    else {
+      x <- TRUE
+    }
+  }
+  return(x)
+}
+
+
+forward_chaining <- function(fact = c()) {
+# assert all known facts (and/or add new)
+  if (!is.contained(fact, facts)){
+    facts[length(facts) + 1] <<- list(fact)
+    is_new <<- TRUE
+  }
+  
+  while(is_new) {
+    is_new <<- FALSE
+    for(assertion in facts) {
+      if(assertion[1] == 'mammal')
+        forward_chaining(c('vertebrate', assertion[2]))
+      if(assertion[1] == 'vertebrate')
+        forward_chaining(c('animal', assertion[2]))
+      if(assertion[1] == 'vertebrate' & is.contained(c('flying', assertion[2]), facts))
+        forward_chaining(c('bird', assertion[2]))
+    }
+    
+  }
+}
 
 
 #=================== TABELA 1 =========================================== 
-# skopiowane ze stacka i przerobione na R (ale nie dziala na razie xd)
 
+# Wnioskowanie w przod
 facts <- list()
-is_changed <- FALSE
-
 facts[[1]] <- list('vertebrate','duck')
 facts[[2]] <- list('flying','duck')
 facts[[3]] <- list('mammal','cat')
 
-assert_fact <- function(fact) {
-  # w tego ifa zawsze wpada, trzeba jakosc inaczej iterowac po zagdniezdzonych listach, pewnie w petli
-  # i powinno by git
-  if (!fact %in% facts){
-    facts[length(facts) + 1] <- list(fact)
-    is_changed = TRUE
-  }
-  
-  while(is_changed) {
-    is_changed = FALSE
-    for(A1 in facts) {
-      if(A1[1] == 'mammal')
-        assert_fact(c('vertebrate', A1[2]))
-      if(A1[1] == 'vertebrate')
-        assert_fact(c('animal', A1[2]))
-      if(A1[1] == 'vertebrate' & c('flying', A1[2]) %in% facts)
-        assert_fact(c('bird', A1[2]))
-    }
-    
-  }
-  print(facts)
-}
-
-assert_fact(facts[[1]])
-
+is_new <- TRUE
+forward_chaining()
+print(facts)
 
 
 
